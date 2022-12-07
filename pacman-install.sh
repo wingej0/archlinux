fff8efe941d57af71ae55a27a2ab07acb7878d51
Jeff Winget <jeff@3dgradebook.com>
2022-12-07 15:05:43 -0700

    snapper tools

--- pacman-install.sh
+++ pacman-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S intel-ucode pcmanfm firefox nitrogen engrampa lxappearance-gtk3 neofetch rofi python-psutil ttf-roboto blueberry polkit-gnome gvfs gparted telegram-desktop code audacity kdenlive obs-studio gnome-keyring xf86-video-intel htop flameshot ncdu bleachbit arandr strawberry galculator picom ttf-font-awesome arc-gtk-theme arc-icon-theme github-cli xfce4-notifyd gimp tumbler evince deja-dup playerctl pavucontrol system-config-printer reflector bluez bluez-utils cups hplip xdg-user-dirs blueman ristretto vlc webp-pixbuf-loader poppler-glib ffmpegthumbnailer freetype2 libgsf totem gnome-epub-thumbnailer firewalld peek caprine meld archlinux-wallpaper zsh
+sudo pacman -S intel-ucode pcmanfm firefox nitrogen engrampa lxappearance-gtk3 neofetch rofi python-psutil ttf-roboto blueberry polkit-gnome gvfs gparted telegram-desktop code audacity kdenlive obs-studio gnome-keyring xf86-video-intel htop flameshot ncdu bleachbit arandr strawberry galculator picom ttf-font-awesome arc-gtk-theme arc-icon-theme github-cli xfce4-notifyd gimp tumbler evince deja-dup playerctl pavucontrol system-config-printer reflector bluez bluez-utils cups hplip xdg-user-dirs blueman ristretto vlc webp-pixbuf-loader poppler-glib ffmpegthumbnailer freetype2 libgsf totem gnome-epub-thumbnailer firewalld peek caprine meld archlinux-wallpaper zsh snapper snap-pac grub-btrfs
 sudo systemctl enable bluetooth
 sudo systemctl enable cups
 sudo systemctl enable firewalld
